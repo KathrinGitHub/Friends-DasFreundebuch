e69e5595d0bc3c886c2ade021d07133285140cb8
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-12-03 23:16:52 +0100

    OwnProfile_Navigation added buttons and listeners

--- app/src/main/java/com/example/friends_dasfreundebuch/OwnProfile.java
+++ app/src/main/java/com/example/friends_dasfreundebuch/OwnProfile.java
@@ -1,8 +1,13 @@
 package com.example.friends_dasfreundebuch;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
@@ -17,5 +22,31 @@ public class OwnProfile extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.own_layout);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        ImageButton editBtn = findViewById(R.id.edit_button);
+        ImageButton backBtn = findViewById(R.id.back_button);
+        ImageButton closeBtn = findViewById(R.id.close_button);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.own_layout_edit_view);
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OwnProfile.this, MainActivity.class));
+            }
+        });
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.own_layout);
+            }
+        });
     }
+
+
 }
