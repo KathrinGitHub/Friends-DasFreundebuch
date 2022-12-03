f8f6a918952cb4da94107220adfbcde56b4d85a3
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-12-03 22:00:19 +0100

    OwnProfile_UI-Spike updated xml and added logic for starting the view

--- app/src/main/java/com/example/friends_dasfreundebuch/OwnProfile.java
+++ app/src/main/java/com/example/friends_dasfreundebuch/OwnProfile.java
@@ -1,6 +1,21 @@
 package com.example.friends_dasfreundebuch;
 
-public class OwnProfile {
-    //shows own profile -> can be edited
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
 
+public class OwnProfile extends ListActivity {
+
+    ArrayList listItems = new ArrayList<>();
+
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.own_layout);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+    }
 }
