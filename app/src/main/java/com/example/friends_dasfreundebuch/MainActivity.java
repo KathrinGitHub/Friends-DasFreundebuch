c933d128bea84b147b5aed7a92cabc495d9611c3
Benj-hub <lamprecht193@gmail.com>
2022-12-05 22:53:08 +0100

    homescreen template

--- app/src/main/java/com/example/friends_dasfreundebuch/MainActivity.java
+++ app/src/main/java/com/example/friends_dasfreundebuch/MainActivity.java
@@ -1,5 +1,16 @@
 package com.example.friends_dasfreundebuch;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     //start screen -> shows list of friends
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
