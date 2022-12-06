a192ede4fe686b638d28c0a80ea9f0c886965f8d
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-12-06 11:24:34 +0100

    FriendProfile_UI-Spike added friend view and necessary navigation;
    correctet some errors so app can be build

--- app/src/main/java/com/example/friends_dasfreundebuch/FriendProfile.java
+++ app/src/main/java/com/example/friends_dasfreundebuch/FriendProfile.java
@@ -1,5 +1,33 @@
 package com.example.friends_dasfreundebuch;
 
-public class FriendProfile {
-    //shows profile of clicked friend
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class FriendProfile extends ListActivity {
+
+    ArrayList listItems = new ArrayList<>();
+
+    ArrayAdapter adapter;
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.own_layout);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        ImageButton backBtn = findViewById(R.id.back_button_friend);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FriendProfile.this, MainActivity.class));
+            }
+        });
+    }
 }
